4d874a4fb78b496bc18f13c840f5de87214adab6
RaffiBienz <42611754+RaffiBienz@users.noreply.github.com>
2020-07-07 11:50:01 +0200

    example data added, multisession + overlap removed

--- dtmanalyzer.R
+++ dtmanalyzer.R
@@ -1,35 +1,33 @@
 ##############################################################################################################################################################
 #### Calculate ground structure from DTM ####
 # Smooths the DTM and then substracts the original DTM. Suitable for finding strip roads in forests and other ground structures.
-# Calculation by 4x4km tiles (to handle memory problems)
 # R.Bienz / 23.07.2019
 ##############################################################################################################################################################
 library(imager)
 library(raster)
 library(rgdal)
-library(future)
-plan(multisession,workers=8L) # for multi core processing
-setwd("...")
-rasterOptions(tmpdir= ".../temp/",todisk=TRUE, progress="text")
+
+wd <- getwd() # Define working directory
+setwd(wd)
+
+dir.create("temp",showWarnings = F)
+dir.create("wd",showWarnings = F)
+dir.create("result",showWarnings = F)
+
+rasterOptions(tmpdir= paste0(wd,"/temp/"),todisk=TRUE, progress="text")
 
 ##############################################################################################################################################################
-#### Import of DTM-Files ####
-dtm_kt <- raster("data/DTM_2019_Milan.tif") # Import DTM for whole area. Used for data extraction by clipping extents.
-files <- list.files(path="data/DTM/",pattern="*.tif$") # DTM tiles 4x4km. They are only used to define the clipping extents 
-for(i in files) {assign(unlist(strsplit(i, "[.]"))[1], raster(paste("data/DTM/",i,sep=""))) } # Load Tiles
-raster_names <- unlist(strsplit(files,"[.]"))
+#### Import DTM-Files ####
+files <- list.files(path="data",pattern="*.tif$") 
+for(i in files) {assign(unlist(strsplit(i, "[.]"))[1], raster(paste("data/",i,sep=""))) } # Load Tiles
+raster_names <- unlist(strsplit(files,"[.]")) # create name list
 raster_names <- raster_names[raster_names!="tif"]
 
 ##############################################################################################################################################################
 #### Calculate structure for each tile with overlap ####
-overlap <- 10 # Define overlap in m to omit edge effects
-
 for(i in 1:length(raster_names)){
-  temp_ras <- eval(as.symbol(raster_names[i]))
-  clip_ext <- extent(temp_ras)
-  clip_ext <- clip_ext +overlap
-  temp <- crop(dtm_kt,clip_ext)
-  
+  temp <- eval(as.symbol(raster_names[i]))
+
   ### Convert to cimg ###
   dtm_img <- as.cimg(t(matrix(temp[],ncol = ncol(temp),byrow = T)))
   dtm_img[is.na(dtm_img)] <- 0
@@ -46,8 +44,8 @@ for(i in 1:length(raster_names)){
   ### Assign coordinate system and export ###
   crs(mag_ras) <- crs(temp)
   extent(mag_ras) <- extent(temp)
-  mag_ras_crop <- crop(mag_ras,extent(temp_ras)) # clip files to original extent
-  writeRaster(mag_ras_crop,filename = paste("wd/",raster_names[i],"_diff.tif",sep=""),overwrite=T)
+  origin(mag_ras) <- c(0,0)
+  writeRaster(mag_ras,filename = paste("wd/",raster_names[i],"_diff.tif",sep=""),overwrite=T)
   
   print(paste(i,"/",length(raster_names)))
 }
@@ -57,18 +55,18 @@ for(i in 1:length(raster_names)){
 #### Read all tiles and build one file ####
 files_diff<-list.files(path="wd/",pattern="*.tif$")
 for(i in files_diff) {assign(unlist(strsplit(i, "[.]"))[1], raster(paste("wd/",i,sep=""))) } 
-diff_namen <- unlist(strsplit(files_diff,"[.]"))
-diff_namen <- diff_namen[diff_namen!="tif"]
+diff_names <- unlist(strsplit(files_diff,"[.]"))
+diff_names <- diff_names[diff_names!="tif"]
 
 raster_list <- list() 
-for (i in diff_namen){
+for (i in diff_names){
   raster_list <- append(raster_list,eval(as.symbol(i)))}
 
 raster_list$fun <- mean
-
-
 mos <- do.call(mosaic, raster_list)
-writeRaster(mos,filename="result/dtm_diff_kt_over.tif",overwrite=T)
+writeRaster(mos,filename="result/dtm_diff_kt.tif",overwrite=T)
 
+# delete temporary files
+unlink("temp", recursive = TRUE)
 
 
